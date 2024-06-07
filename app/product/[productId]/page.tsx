e00329df3d9854717a7925e2f26065ea0aeba749
Ali Afarinifard <aliafarinifard@outlook.com>
2024-06-07 23:48:38 +0330

    ...

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -3,8 +3,6 @@ import { getCurrentUser } from "@/actions/getCurrentUser";
 import Container from "@/app/components/Container"
 import NullData from "@/app/components/NullData"
 import ProductDetails from "./ProductDetails"
-import ListRating from "./ListRating";
-import AddRating from "./AddRating";
 
 interface IParams {
     productId?: string
@@ -26,14 +24,6 @@ const Product = async ({ params }: { params: IParams }) => {
         <div className="p-8">
             <Container>
                 <ProductDetails product={product} />
-
-                <div
-                    className="flex flex-col mt-20 gap-4"
-                >
-                    <AddRating product={product} user={user} />
-
-                    <ListRating product={product} />
-                </div>
             </Container>
         </div>
     )
